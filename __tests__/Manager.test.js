2b8803200f9154b272b71b8a66133b0e3ed53264
trm621 <t.maher621@gmail.com>
2021-12-01 18:13:54 -0500

    built manager object with passing tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,12 @@
 const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
 
 test("checks for office number", () => {
     const manager = new Manager()
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
+
+test('checks to see if role is switched', () => {
+    const manager = new Manager()
+    expect(manager.role).toEqual("Manager");
+})
