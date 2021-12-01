da828e399b1c2a0372b69cf3ac06b673d0c1a4f4
trm621 <t.maher621@gmail.com>
2021-12-01 16:00:43 -0500

    built Employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,27 +1,33 @@
-const { expect } = require('@jest/globals');
+const { handlePotentialSyntaxError } = require('@jest/transform');
 const Employee = require('../lib/Employee');
 
 test('checks to see if an employee object is made', () => {
-    const employee = new Employee();
-    expect(employee.name).toEqual(expect.any(String));
+    const employee = new Employee('Dave');
+    expect(employee.name).toEqual('Dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
 
 test("gets the employee's name", () => {
-    const employee = new Employee();
+    const employee = new Employee('Dave');
 
-    expect(employee.getName()).toEqual(expect.any(String))
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name))
 });
 
 test("gets the employee's id number", () => {
-    const employee = new Employee();
+    const employee = new Employee('Dave');
 
-    expect(employee.getEmail()).toEqual(expect.any(String));
+    expect(employee.getId()).toEqual(expect.any(Number));
 });
 
-test("gets employee's full description", () => {
-    const employee = new Employee();
+test("gets the employee's email address", () => {
+    const employee = new Employee('Dave');
 
-    expect(employee.getEmail()).toEqual(expect.any(Object));
+    expect(employee.getEmail()).toEqual(expect.any(String));
+})
+
+test("gets employee's description", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
 });
