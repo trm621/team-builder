f98ff173d538ca964e23de9f773312f47af107eb
trm621 <t.maher621@gmail.com>
2021-12-01 19:16:12 -0500

    set up index file

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquierer');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
