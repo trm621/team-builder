14063b214680c855b93232b52db4af28b6075056
trm621 <t.maher621@gmail.com>
2021-12-05 20:43:36 -0500

    fixed objects

--- index.js
+++ index.js
@@ -1,86 +1,48 @@
-const inquirer = require('inquirer');
-const Engineer = require('./lib/Engineer');
-const Manager = require('./lib/Manager');
-const Intern = require('./lib/Intern');
+// const inquirer = require('inquirer');
+// const Engineer = require('./lib/Engineer');
+// const Manager = require('./lib/Manager');
+// const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
-const fs = require('fs');
-const generatePage = require('./utils/generate-page');
+// const fs = require('fs');
+// const generatePage = require('./utils/generate-page');
+// const teamMates = [];
 
-const initializeApp = teamMates => {
-    if (!teamMates) {
-        teamMates = [];
-    }
-    return inquirer
-        .prompt([{
-            type: 'input',
-            name: 'name',
-            message: "Please enter a teammate's name."
-        },
-        {
-            type: 'number',
-            name: 'id',
-            message: "Please enter your teammate's ID number",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Please enter your teammate's email address."
-        },
-        {
-            type: 'checkbox',
-            name: 'role',
-            message: "Please select your teammate's role.",
-            choices: ["Manager", "Intern", "Engineer"]
-        }])
-        .then(newEmployee => {
-            teamMates.push(newEmployee)
-            return newEmployee,
-            createNewEmployee(newEmployee)
-        })
-    };
+new Employee().initializeApp();
 
-const createNewEmployee = (newEmployee) => {
-    if (newEmployee === "Manager") {
-        console.log("Manager created!");
-    }
-}
 
-writeToFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/index.html', fileContent, err => {
-        if (err) {
-            reject(err);
-            return;
-        }
+// writeToFile = fileContent => {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile('./dist/index.html', fileContent, err => {
+//         if (err) {
+//             reject(err);
+//             return;
+//         }
       
-        resolve({
-            ok: true,
-            message: 'File created!'
-        });
-    });
-});
-};
+//         resolve({
+//             ok: true,
+//             message: 'File created!'
+//         });
+//     });
+// });
+// };
 
-const copyFile = () => {
-    return new Promise((resolve, reject) => {
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-          reject(err);
-          return;
-        }
+// const copyFile = () => {
+//     return new Promise((resolve, reject) => {
+//       fs.copyFile('./src/style.css', './dist/style.css', err => {
+//         if (err) {
+//           reject(err);
+//           return;
+//         }
   
-        resolve({
-          ok: true,
-          message: 'Stylesheet created!'
-        });
-      });
-    });
-  };
+//         resolve({
+//           ok: true,
+//           message: 'Stylesheet created!'
+//         });
+//       });
+//     });
+//   };
 
-initializeApp()
-  .then(newEmployee => {
-      return createNewEmployee(newEmployee)
-  })
+// initializeApp();
     
 // .then(teamMateInfo => {
 //     return generatePage(teamMateInfo);
